6f192979dfb5e70455df99e99f182ddf7292e0ca
Jim Nervall <nervall@hotmail.com>
2024-09-03 15:12:19 +0200

    Added Input Component

---
+++ src/components/input.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent } from 'react';
+import './input.css';
+
+type InputProps = {
+    className?: string;
+    type: string;
+    name: string;
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement> ) => void;
+};
+
+const Input = ({ className, type, name, value, onChange}: InputProps) => {
+  return (
+    <input
+        className={`input-wrapper ${className}`}
+        type={type}
+        name={name}
+        maxLength={40}
+        minLength={2}
+        onChange={(e) => onChange(e)}
+        value={value}
+        >
+    </input>
+  )
+};
+
+export default Input;
